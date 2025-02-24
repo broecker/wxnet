31faa8cc4dffaf3dfc0363a2b72f6e21e747eee6
Markus Broecker <mbrckr@gmail.com>
2025-02-23 19:01:04 -0700

    Improved json -> csv conversion slightly. Laid groundwork for training data

--- src/collate_measurements.py
+++ src/collate_measurements.py
@@ -8,6 +8,16 @@ import json
 import logging
 import pathlib
 
+_HISTORY_LENGTH = flags.DEFINE_integer(
+  'history_length', 7,
+  'How many days into the past should the history run? A longer history gives '
+  'more training data but fewere datapoints to train with.')
+
+_PREDICTION_LENGTH = flags.DEFINE_integer(
+  'prediction_length', 2,
+  'How many days into the future should we try collect outcomes?'
+)
+
 
 @dataclasses.dataclass
 class Measurement:
@@ -21,6 +31,16 @@ class Measurement:
   
   def get_csv_line(self) -> str:
     return f'{self.timestamp.isoformat()},{self.humidity},{self.temperature},{self.pressure}\n'
+
+@dataclasses.dataclass
+class TrainingSet:
+  """A single line in the training set. Contains the history, current and future Measurements."""
+  # The current measurement.
+  actual: Measurement
+  # The history, defined by --history_length, of measurments leading to this.
+  history: list[Measurement]
+  # Future measurements, defined by --prediction_length.
+  future: list[Measurement]
 
 
 def _fahrenheit_to_celsius(f: float) -> float:
@@ -44,14 +64,15 @@ def _write_csv(data: list[Measurement], outfile: pathlib.Path) -> None:
 
 
 def main(argv: list[str]) -> None:
+  if len(argv) != 2:
+    raise app.UsageError(f'Invalid args; usage: {argv[0]} <input file> [optional flags]')
+  
   input_file = pathlib.Path(argv[1])
-  output_file = pathlib.Path(argv[2])
   if not input_file.exists():
     raise FileNotFoundError(input_file)
 
-  logging.info('Reading from %s, writing to %s', input_file, output_file)
-  
   with open(input_file, 'r') as f:
+    logging.info('Reading from %s', input_file)
     data = json.load(f)
   
   start = datetime.datetime.fromtimestamp(data['start_timestamp'])
@@ -73,10 +94,9 @@ def main(argv: list[str]) -> None:
   logging.info('Sorting %d entries', len(cleaned_data))
   cleaned_data = sorted(cleaned_data, key=lambda d: d.timestamp)
 
-  _write_csv(cleaned_data, output_file)
-
+  raw_csv = input_file.with_suffix('.csv')
+  _write_csv(cleaned_data, raw_csv)
   
-
 
 if __name__ == '__main__':
   app.run(main)
